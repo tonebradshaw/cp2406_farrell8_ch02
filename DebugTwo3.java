a04ab0a563bac5ecea36d2bb3eb26f127b606875
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-08-08 08:00:15 +1000

    prac2 files

---
+++ DebugTwo3.java
@@ -0,0 +1,17 @@
+/**
+ * Created by tony on 7/08/2016.
+ */
+public class DebugTwo3 {
+
+    // Demonstrates remainder and output
+
+    public static void main(String args[]){
+        int a = 99, b = 8, result;
+        long c = 7777777777777l;
+        result = a % b;
+        System.out.println("Divide " + a + " by " + b);
+        System.out.println("remainder is " + result);
+        System.out.print("c is a very large number: ");
+        System.out.println(c);
+    }
+}
